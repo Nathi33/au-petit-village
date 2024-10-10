c87fde39722b8ef327d56907dc05b4117a81921b
Nathi33 <nathaliedarnaudat@outlook.com>
2024-10-10 14:20:41 +0200

    add contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -39,7 +40,7 @@ registerLocaleData(localeFr);
     FooterComponent,
     UnitCmPipe,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }],
   bootstrap: [AppComponent],
 })
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
+  formulaire: FormGroup;
+  messageEnvoye: boolean = false;
+  // Permet de suivre l'état de soumission du formulaire
+  submitted: boolean = false;
 
+  constructor(private fb: FormBuilder) {
+    // Création du formulaire
+    this.formulaire = this.fb.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+    });
+  }
+
+  // Méthode appelée lors de la soumission du formulaire
+  onSubmit() {
+    //Marque le formulaire comme soumis
+    this.submitted = true;
+
+    if (this.formulaire.valid) {
+      // Affichage du message de confirmation
+      this.messageEnvoye = true;
+
+      // Réinitialisation du formulaire
+      setTimeout(() => {
+        this.formulaire.reset();
+        this.submitted = false;
+        this.messageEnvoye = false;
+      }, 3000);
+    }
+  }
+
+  // Méthode pour faciliter l'affichage des erreurs
+  get f() {
+    return this.formulaire.controls;
+  }
 }
